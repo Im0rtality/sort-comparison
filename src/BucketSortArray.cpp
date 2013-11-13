f241305aba05810ca561bcf648b5635c884a84cb
Laurynas Verzukauskas <laurynas@whatever.com>
2013-11-13 02:25:48 +0200

    BucketSort fixes

--- src/BucketSortArray.cpp
+++ src/BucketSortArray.cpp
@@ -1,10 +1,11 @@
 #include "StdAfx.h"
 #include "BucketSortArray.h"
 #include "FileArrayInterface.h"
-#include "MemoryArrayInterface.h"
 #include <queue>
 
 using namespace std;
+
+const int bucket_count = 33;
 
 void BucketSortArray::run(void)
 {
@@ -13,7 +14,7 @@ void BucketSortArray::run(void)
 
 int BucketSortArray::hash(int n)
 {
-    return n / 5;
+    return n % 33;
 }
 
 void BucketSortArray::sortBucket(FileArrayInterface * input, int len)
@@ -30,39 +31,39 @@ void BucketSortArray::sortBucket(FileArrayInterface * input, int len)
 
 void BucketSortArray::bucketsort()
 {
-    queue < unsigned char >*buckets[this->num];
-    for (int i = 0; i < this->num; i++)
-	buckets[i] = new queue < unsigned char >;
+    queue < unsigned char >*buckets[bucket_count];
+    for (int i = 0; i < bucket_count; i++)
+		buckets[i] = new queue < unsigned char >;
 
     // Hash the input and insert the content into appropriate bucket based on hash.
-    for (int i = 0; i < this->num; i++) {
-	int hashValue = this->hash(this->a->get(i));
-	if (hashValue >= this->num)
-	    hashValue = this->num - 1;
+	for (int i = 0; i < this->num; i++) {
+		int hashValue = this->hash(this->a->get(i));
+		if (hashValue >= this->num)
+			hashValue = this->num - 1;
 
-	buckets[hashValue]->push(this->a->get(i));
+		buckets[hashValue]->push(this->a->get(i));
     }
 
     // extract the content from each of the buckets in order.
     // using insertion sort
     int outputidx = 0;
-    for (int i = 0; i < this->num; i++) {
-	if (buckets[i]->size() == 1) {
-	    this->a->set(outputidx++, buckets[i]->front());
-	    buckets[i]->pop();
-	}
-	if (buckets[i]->size() > 1) {
-	    while (!buckets[i]->empty()) {
-		this->a->set(outputidx, buckets[i]->front());
+    for (int i = 0; i < bucket_count; i++) {
+		if (buckets[i]->size() == 1) {
+			this->a->set(outputidx++, buckets[i]->front());
+			buckets[i]->pop();
+		}
+		if (buckets[i]->size() > 1) {
+			while (!buckets[i]->empty()) {
+				this->a->set(outputidx, buckets[i]->front());
 
-		this->sortBucket(this->a, outputidx);
-		buckets[i]->pop();
-		outputidx++;
-	    }
-	}
+				this->sortBucket(this->a, outputidx);
+				buckets[i]->pop();
+				outputidx++;
+			}
+		}
     }
     // clear buckets.
-    for (int i = 0; i < this->num; i++)
-	delete buckets[i];
+    for (int i = 0; i < bucket_count; i++)
+		delete buckets[i];
 
 }
